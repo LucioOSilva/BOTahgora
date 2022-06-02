3c636f6a0be5f986121febe718634adb39952adb
lucioosilva <lucioosilva00@gmail.com>
2022-06-02 19:18:31 -0300

    Updates anything

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,10 @@ const {
   setStartUpAppTimeStamp,
   getStartUpAppDateBR,
   getStartUpAppHourComplete,
-  logReader,
+  
   logWriter,
-  baseHourReader
+  baseHourReader,
+  dateReader
 } = require('../Services');
 
 
@@ -17,11 +18,45 @@ const startUpDate = () => {
 const calculateNextPunches = async () => {
   const [day, month, year] = getStartUpAppDateBR().split('/');
   const [hour, minute, second] = getStartUpAppHourComplete().split(':');
+  const datesArray = await dateReader();
   const { minutesVariation, hourCompanyPreset } = await baseHourReader();
+  // setTimeout(() => {
+  //   console.log('timeout1500')
+  // }, 1500);
+  // setTimeout(() => {
+  //   console.log('timeout2000')
+  // }, 2000);
+
+  // setTimeout(() => {
+  //   console.log('timeout3500')
+  // }, 2500);
+  // setTimeout(() => {
+  //   console.log('timeout3000')
+  // }, 3000);
+  // setTimeout(() => {
+  //   console.log('timeout4500')
+  // }, 3500);
+  // setTimeout(() => {
+  //   console.log('timeout4000')
+  // }, 4000);
+  // setTimeout(() => {
+  //   console.log('timeout5500')
+  // }, 4500);
+  // setTimeout(() => {
+  //   console.log('timeout6000')
+  // }, 5000);
+  // setTimeout(() => {
+  //   console.log('timeout6500')
+  // }, 5500);
+  // setTimeout(() => {
+  //   console.log('timeout6000')
+  // }, 6000);
+  
+  // console.log(datesArray)
 
 
-  console.log(day, month, year, hour, minute, second)
-  const d = new Date(day, month, year, hour, minute, second)
+  // console.log(day, month, year, hour, minute, second)
+  // const d = new Date(day, month, year, hour, minute, second)
   
   
 }
