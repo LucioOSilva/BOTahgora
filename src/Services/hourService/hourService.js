306e791969db3125db2fc61756f5abe89aaad7f1
lucioosilva <lucioosilva00@gmail.com>
2022-06-07 17:11:48 -0300

    Updateas

--- src/Services/hourService/hourService.js
+++ src/Services/hourService/hourService.js
@@ -12,3 +12,31 @@ module.exports = {
   getStartUpAppDateBR,
   getStartUpAppHourComplete,
 };
+
+
+// import {TimeSplit} from '../typings/time'
+
+// export function parseTimeRemaining (totalSeconds: number , hasOffer:boolean): TimeSplit  {
+//     const days = Math.floor (totalSeconds/ (24 * 60 * 60 * 1000))
+//     const hours = Math.floor(totalSeconds/ (60 * 60 * 1000));
+//     const minutes = Math.floor(totalSeconds/ (60 * 1000));
+//     const seconds = Math.floor(totalSeconds / 1000);
+
+//   return {
+//     days:days,
+//     hours: (hours % 24),
+//     minutes: minutes % 60,
+//     seconds: seconds % 60,
+//     hasOffer:hasOffer
+//   }
+// }
+
+// export function timeOffer( dateFinal:string){
+//     const start =  new Date().getTime()
+//     const final =  new Date(dateFinal).getTime()
+
+//     const timeStampDiff =  (final) - start
+//     const secondsRemaining = timeStampDiff
+//     const hasOffer = secondsRemaining> 0
+//     return parseTimeRemaining(secondsRemaining, hasOffer)
+// }
